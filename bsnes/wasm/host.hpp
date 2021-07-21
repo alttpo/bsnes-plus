5a23c7f6fd85ad103525ca8fa2b38aee79b66738
jsd1982 <james.jdunne@gmail.com>
2021-07-21 14:47:10 -0500

    wasm: remove wasm3_cpp.h since it uses too-new C++17 features which do not mix with the gnu++0x codebase

--- bsnes/wasm/host.hpp
+++ bsnes/wasm/host.hpp
@@ -1,23 +1,29 @@
 #pragma once
 
 #include <optional>
+#include <vector>
 
-#include "wasm3_cpp.h"
+#include "wasm3.h"
 
 namespace WASM {
 
 struct Host {
-  Host(size_t stack_size) : default_stack_size(stack_size), env() {}
+  Host(size_t stack_size_bytes) : default_stack_size_bytes(stack_size_bytes) {
+    m_env.reset(m3_NewEnvironment(), m3_FreeEnvironment);
+    if (m_env == nullptr) {
+      throw std::bad_alloc();
+    }
+  }
 
   void reset();
   void add_module(const uint8_t *data, size_t size);
-  void invoke_all(const char *name, std::function<void(wasm3::function&)> cb);
+  void invoke_all(const char *name, std::function<void(M3Function*)> each);
 
 public:
-  size_t default_stack_size;
-  wasm3::environment env;
+  size_t default_stack_size_bytes;
 
-  std::vector<wasm3::runtime> runtimes;
+  std::shared_ptr<struct M3Environment>          m_env;
+  std::vector<std::shared_ptr<struct M3Runtime>> m_runtimes;
 };
 
 extern Host host;
