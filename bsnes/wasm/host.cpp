76060d20d6c2e8b48c869e915b27da070bf793db
jsd1982 <james.jdunne@gmail.com>
2021-07-20 22:18:52 -0500

    wasm: basic Host class to continually parse and load modules into their own dedicated runtimes

--- bsnes/wasm/host.cpp
+++ bsnes/wasm/host.cpp
@@ -1,12 +1,27 @@
-#include "wasm3.h"
+#include "host.hpp"
 
-namespace wasm {
+namespace WASM {
 
-class Host {
-public:
-  IM3Environment env;
-};
+Host host(1024);
 
-Host host;
+void Host::reset() {
+  runtimes.clear();
+}
+
+void Host::add_module(const uint8_t *data, size_t size) {
+  wasm3::module mod = env.parse_module(data, size);
+
+  auto runtime = env.new_runtime(default_stack_size);
+  runtime.load(mod);
+
+  runtimes.push_back(runtime);
+}
+
+void Host::invoke_all(const char *name, std::function<void(wasm3::function&)> cb) {
+  for (auto &runtime: runtimes) {
+    auto func = runtime.find_function(name);
+    cb(func);
+  }
+}
 
 }
