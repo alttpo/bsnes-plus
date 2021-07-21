512d90c5bb965131fac99077960df712d901b6a5
jsd1982 <james.jdunne@gmail.com>
2021-07-21 16:14:15 -0500

    wasm: error checking; added link(); tracking modules

--- bsnes/wasm/host.hpp
+++ bsnes/wasm/host.hpp
@@ -7,6 +7,10 @@
 
 namespace WASM {
 
+struct error : public std::runtime_error {
+  explicit error(M3Result err) : std::runtime_error(err) {}
+};
+
 struct Host {
   Host(size_t stack_size_bytes) : default_stack_size_bytes(stack_size_bytes) {
     m_env.reset(m3_NewEnvironment(), m3_FreeEnvironment);
@@ -17,13 +21,15 @@ struct Host {
 
   void reset();
   void add_module(const uint8_t *data, size_t size);
-  void invoke_all(const char *name, std::function<void(M3Function*)> each);
+  void link(const char *module_name, const char *function_name, const char *signature, M3RawCall rawcall);
+  void invoke_all(const char *name, int argc, const char* argv[]);
 
 public:
   size_t default_stack_size_bytes;
 
-  std::shared_ptr<struct M3Environment>          m_env;
-  std::vector<std::shared_ptr<struct M3Runtime>> m_runtimes;
+  std::shared_ptr<struct M3Environment>           m_env;
+  std::vector<std::shared_ptr<struct M3Runtime>>  m_runtimes;
+  std::vector<std::shared_ptr<struct M3Module>>   m_modules;
 };
 
 extern Host host;
