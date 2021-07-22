02ed89d4a1abf4c989c32bf8eeae900ee423890a
jsd1982 <james.jdunne@gmail.com>
2021-07-22 00:18:18 -0500

    wasm: export ppux_sprite_write() with working test case

---
+++ bsnes/wasm/test/send-wasm.sh
@@ -0,0 +1,4 @@
+echo -ne 'WASM_RESET\nPPUX_RESET\n' | socat stdio tcp:127.0.0.1:65400
+# TODO: update \x01\x95 to length of test.wasm file
+(echo -ne 'WASM_ADD\n\x00\x00\x00\x01\x95'; cat test.wasm) | socat stdio tcp:127.0.0.1:65400
+echo -ne 'WASM_INVOKE on_nmi\n' | socat stdio tcp:127.0.0.1:65400
