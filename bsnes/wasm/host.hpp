c913a45e2149170cbd36c112dc4aa70f193db2e8
jsd1982 <james.jdunne@gmail.com>
2021-07-21 21:24:52 -0500

    wasm: add linkEx to allow pointer-to-member function linking

--- bsnes/wasm/host.hpp
+++ bsnes/wasm/host.hpp
@@ -11,6 +11,17 @@ struct error : public std::runtime_error {
   explicit error(M3Result err) : std::runtime_error(err) {}
 };
 
+template <class T>
+struct RawCall;
+
+template <class T>
+struct RawCall<const void * (T::*) (IM3Runtime runtime, IM3ImportContext _ctx, uint64_t * _sp, void * _mem)> {
+  template <const void * (T::*Func) (IM3Runtime runtime, IM3ImportContext _ctx, uint64_t * _sp, void * _mem)>
+  static const void * adapter(IM3Runtime runtime, IM3ImportContext _ctx, uint64_t * _sp, void * _mem) {
+    return (reinterpret_cast<T*>(_ctx->userdata)->*Func)(runtime, _ctx, _sp, _mem);
+  }
+};
+
 struct Host {
   Host(size_t stack_size_bytes) : default_stack_size_bytes(stack_size_bytes) {
     m_env.reset(m3_NewEnvironment(), m3_FreeEnvironment);
@@ -22,6 +33,7 @@ struct Host {
   void reset();
   void add_module(const uint8_t *data, size_t size);
   void link(const char *module_name, const char *function_name, const char *signature, M3RawCall rawcall);
+  void linkEx(const char *module_name, const char *function_name, const char *signature, M3RawCall rawcall, const void *userdata);
   void invoke_all(const char *name, int argc, const char* argv[]);
 
 public:
