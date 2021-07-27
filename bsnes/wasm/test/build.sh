75f5842dbd184e9862b1f2ca4c6d8f5733a79fe9
jsd1982 <james.jdunne@gmail.com>
2021-07-27 10:01:17 -0500

    wasm: test code updated to split defs to wasm.h; added 3bpp decompression routine

--- bsnes/wasm/test/build.sh
+++ bsnes/wasm/test/build.sh
@@ -3,12 +3,11 @@ file="test.wasm"
 /usr/local/opt/llvm/bin/clang \
   --target=wasm32 \
   -O3 \
-  -flto \
   -nostdlib \
   -nostartfiles \
+  -fno-builtin \
   -Wl,--no-entry \
   -Wl,--export-all \
-  -Wl,--lto-O3 \
   -Wl,-z,stack-size=$[1048576] \
   -o "${file}" \
   test.c
