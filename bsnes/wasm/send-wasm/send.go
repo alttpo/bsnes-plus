25252a1d2dadad37709b30f6064f6c311945f1b2
jsd1982 <james.jdunne@gmail.com>
2021-08-04 22:24:52 -0500

    send-wasm: flexible wasm runtime and module upload

--- bsnes/wasm/send-wasm/send.go
+++ bsnes/wasm/send-wasm/send.go
@@ -3,14 +3,30 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"net"
 	"os"
+	"path/filepath"
 	"time"
 )
 
 func main() {
 	var err error
+
+	fReset := flag.Bool("reset", false, "Reset WASM")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 2 {
+		fmt.Println("send-wasm [-reset] <runtime name> <wasm file> <wasm file> ...")
+		return
+	}
+
+	runtimeName := args[0]
+	args = args[1:]
+
 	var c *net.TCPConn
 	var addr *net.TCPAddr
 	addr, err = net.ResolveTCPAddr("tcp", "127.0.0.1:65400")
@@ -23,43 +39,58 @@ func main() {
 		panic(err)
 	}
 
-	var fb []byte
-	fb, err = ioutil.ReadFile("test.wasm")
-	if err != nil {
-		panic(err)
-	}
-
-	b := bytes.Buffer{}
-	b.Grow(len(fb) + 20)
-	b.WriteString("WASM_RESET\nWASM_LOAD test;test\n")
-	b.WriteByte(0)
-	err = binary.Write(&b, binary.BigEndian, uint32(len(fb)))
-	if err != nil {
-		panic(err)
-	}
 	var n int
-	n, err = b.Write(fb)
-	if err != nil {
-		panic(err)
+	b := bytes.Buffer{}
+	if *fReset {
+		b.WriteString("WASM_RESET\n")
+		_, err = c.Write(b.Bytes())
+		if err != nil {
+			panic(err)
+		}
 	}
 
-	//fmt.Print(hex.Dump(b.Bytes()))
-	_, err = c.Write(b.Bytes())
-	if err != nil {
-		panic(err)
-	}
+	for _, path := range args {
+		var fb []byte
+		fb, err = ioutil.ReadFile(path)
+		if err != nil {
+			panic(err)
+		}
 
-	b.Reset()
-	err = c.SetReadDeadline(time.Now().Add(time.Second))
-	if err != nil {
-		panic(err)
-	}
-	rsp := make([]byte, 65536)
-	n, err = c.Read(rsp)
-	if err != nil {
-		panic(err)
-	}
-	rsp = rsp[:n]
+		b.Reset()
+		b.Grow(len(fb) + 20)
 
-	_, _ = os.Stdout.Write(rsp)
+		// strip extension off filename:
+		moduleName := filepath.Base(path)
+		moduleName = moduleName[0:len(moduleName)-len(filepath.Ext(moduleName))]
+
+		_, _ = fmt.Fprintf(&b, "WASM_LOAD %s;%s\n", runtimeName, moduleName)
+		b.WriteByte(0)
+		err = binary.Write(&b, binary.BigEndian, uint32(len(fb)))
+		if err != nil {
+			panic(err)
+		}
+		n, err = b.Write(fb)
+		if err != nil {
+			panic(err)
+		}
+
+		//fmt.Print(hex.Dump(b.Bytes()))
+		_, err = c.Write(b.Bytes())
+		if err != nil {
+			panic(err)
+		}
+
+		err = c.SetReadDeadline(time.Now().Add(time.Second))
+		if err != nil {
+			panic(err)
+		}
+		rsp := make([]byte, 65536)
+		n, err = c.Read(rsp)
+		if err != nil {
+			panic(err)
+		}
+		rsp = rsp[:n]
+
+		_, _ = os.Stdout.Write(rsp)
+	}
 }
