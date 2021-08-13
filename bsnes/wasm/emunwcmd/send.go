2e547217076dcd372045345b0284ef5f44eabd1f
jsd1982 <james.jdunne@gmail.com>
2021-08-12 21:40:05 -0500

    emunwcmd: refactor to allow sending multiple commands in the form "command;args;args@file"; add -async flag to send all commands at once and await responses

--- bsnes/wasm/emunwcmd/send.go
+++ bsnes/wasm/emunwcmd/send.go
@@ -15,20 +15,13 @@ import (
 func main() {
 	var err error
 
-	fData := flag.String("data", "", "Send binary data with command from file")
+	async := flag.Bool("async", false, "send all commands and then await all responses afterwards")
 	flag.Parse()
 
 	args := flag.Args()
 	if len(args) < 1 {
-		fmt.Println("emunwcmd [-data <file>] <command> [args...]")
+		fmt.Println("emunwcmd <command>[;args;args...][@file]")
 		return
-	}
-
-	cmd := args[0]
-	if len(args) > 1 {
-		args = args[1:]
-	} else {
-		args = args[:0]
 	}
 
 	var c *net.TCPConn
@@ -43,54 +36,87 @@ func main() {
 		panic(err)
 	}
 
-	var n int
-	b := bytes.Buffer{}
-	b.Grow(len(cmd) + 1)
-	b.WriteString(cmd)
-	if len(args) > 0 {
-		argStr := strings.Join(args, ";")
-		b.Grow(len(argStr) + 1)
-		_, _ = fmt.Fprintf(&b, " %s", argStr)
-	}
-	b.WriteByte('\n')
+	for _, combo := range args {
+		path := ""
+		at := strings.LastIndex(combo, "@")
+		if at >= 0 {
+			path = combo[at+1:]
+			combo = combo[0:at]
+		}
 
-	path := *fData
-	if path != "" {
-		var fb []byte
-		fb, err = ioutil.ReadFile(path)
+		cmdargs := strings.Split(combo, ";")
+		cmd := cmdargs[0]
+		args := cmdargs[1:]
+
+		b := bytes.Buffer{}
+		b.Grow(len(cmd) + 1)
+		b.WriteString(cmd)
+		if len(args) > 0 {
+			argStr := strings.Join(args, ";")
+			b.Grow(len(argStr) + 1)
+			_, _ = fmt.Fprintf(&b, " %s", argStr)
+		}
+		b.WriteByte('\n')
+
+		if path != "" {
+			var fb []byte
+			fb, err = ioutil.ReadFile(path)
+			if err != nil {
+				panic(err)
+			}
+
+			b.Grow(1 + 4 + len(fb))
+
+			b.WriteByte(0)
+			err = binary.Write(&b, binary.BigEndian, uint32(len(fb)))
+			if err != nil {
+				panic(err)
+			}
+
+			_, err = b.Write(fb)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		//fmt.Print(hex.Dump(b.Bytes()))
+		_, err = c.Write(b.Bytes())
 		if err != nil {
 			panic(err)
 		}
 
-		b.Grow(1 + 4 + len(fb))
+		if !*async {
+			err = c.SetReadDeadline(time.Now().Add(time.Second))
+			if err != nil {
+				panic(err)
+			}
+			rsp := make([]byte, 65536)
 
-		b.WriteByte(0)
-		err = binary.Write(&b, binary.BigEndian, uint32(len(fb)))
+			var n int
+			n, err = c.Read(rsp)
+			if err != nil {
+				panic(err)
+			}
+			rsp = rsp[:n]
+
+			_, _ = os.Stdout.Write(rsp)
+		}
+	}
+
+	if *async {
+		err = c.SetReadDeadline(time.Now().Add(time.Second))
 		if err != nil {
 			panic(err)
 		}
-		n, err = b.Write(fb)
+		rsp := make([]byte, 65536)
+
+		var n int
+		n, err = c.Read(rsp)
 		if err != nil {
 			panic(err)
 		}
-	}
+		rsp = rsp[:n]
 
-	//fmt.Print(hex.Dump(b.Bytes()))
-	_, err = c.Write(b.Bytes())
-	if err != nil {
-		panic(err)
+		_, _ = os.Stdout.Write(rsp)
 	}
-
-	err = c.SetReadDeadline(time.Now().Add(time.Second))
-	if err != nil {
-		panic(err)
-	}
-	rsp := make([]byte, 65536)
-	n, err = c.Read(rsp)
-	if err != nil {
-		panic(err)
-	}
-	rsp = rsp[:n]
-
-	_, _ = os.Stdout.Write(rsp)
 }
--- bsnes/wasm/test/test.sh
+++ bsnes/wasm/test/test.sh
@@ -1,4 +1,1 @@
-emunwcmd PPUX_RESET
-emunwcmd WASM_RESET
-emunwcmd -data kakwafont-12-n.pcf PPUX_FONT_SET_PCF 0
-emunwcmd -data test.wasm WASM_LOAD test test
+emunwcmd -async 'PPUX_RESET' 'WASM_RESET' 'PPUX_FONT_SET_PCF;0@kakwafont-12-n.pcf' 'WASM_LOAD;test;test@test.wasm'
