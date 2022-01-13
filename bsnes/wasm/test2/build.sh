8c6995220b074c3cf108755fc984b067f943cff9
James Dunne <james.jdunne@gmail.com>
2022-01-13 13:27:19 -0600

    wasm: Go wasm test

--- bsnes/wasm/test2/build.sh
+++ bsnes/wasm/test2/build.sh
@@ -1,1 +1,1 @@
-tinygo build -gc=none -wasm-abi=generic -target=wasm -no-debug -opt=s -o test2.wasm test2.go
+tinygo build -gc=none -wasm-abi=generic -target=wasm -no-debug -opt=s -o main.wasm test2.go
--- bsnes/wasm/test2/test2.go
+++ bsnes/wasm/test2/test2.go
@@ -5,8 +5,43 @@ import "unsafe"
 //export ppux_reset
 func ppux_reset()
 
+//export ppux_draw_list_reset
+func ppux_draw_list_reset()
+
+//export ppux_draw_list_append
+func ppux_draw_list_append(layer, priority uint8, size uint32, cmdlist unsafe.Pointer)
+
+const (
+	COLOR_STROKE = iota
+	COLOR_FILL
+	COLOR_OUTLINE
+
+	COLOR_MAX
+)
+
+const color_none uint16 = 0x8000
+
+const (
+	// commands which ignore state:
+	CMD_VRAM_TILE = iota
+	CMD_IMAGE
+	// commands which affect state:
+	CMD_COLOR_DIRECT_BGR555
+	CMD_COLOR_DIRECT_RGB888
+	CMD_COLOR_PALETTED
+	CMD_FONT_SELECT
+	// commands which use state:
+	CMD_TEXT_UTF8
+	CMD_PIXEL
+	CMD_HLINE
+	CMD_VLINE
+	CMD_LINE
+	CMD_RECT
+	CMD_RECT_FILL
+)
+
 //export snes_bus_read
-func snes_bus_read(i_address uint32, i_data unsafe.Pointer, i_size uint32);
+func snes_bus_read(i_address uint32, i_data unsafe.Pointer, i_size uint32)
 
 //export on_nmi
 func on_nmi() {
@@ -14,4 +49,11 @@ func on_nmi() {
 
 	var module uint8
 	snes_bus_read(0x7E0010, unsafe.Pointer(&module), 1)
+
+	var cmd [8]uint16 = [8]uint16{
+		3, CMD_COLOR_DIRECT_BGR555, COLOR_STROKE, 0x1F3F,
+		3, CMD_PIXEL, 18, 118,
+	}
+	ppux_draw_list_reset()
+	ppux_draw_list_append(4, 15, uint32(len(cmd)), unsafe.Pointer(&cmd))
 }
