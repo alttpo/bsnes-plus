3bc3d5ef39dfad2c53d92597de35ff277adf6afd
James Dunne <james.jdunne@gmail.com>
2022-01-14 00:04:29 -0600

    wasm: test2 build.sh script should now hopefully compile Go code more correctly

--- bsnes/wasm/test2/build.sh
+++ bsnes/wasm/test2/build.sh
@@ -1,1 +1,2 @@
-tinygo build -gc=none -wasm-abi=generic -target=wasm -no-debug -opt=s -o main.wasm test2.go
+#tinygo build -gc=none -wasm-abi=generic -target=wasm -no-debug -opt=s -o main.wasm test2.go
+tinygo build -gc=none -wasm-abi=generic -target=wasm -scheduler=none -panic=trap -o main.wasm test2.go
