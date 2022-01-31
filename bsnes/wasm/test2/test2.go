8c650aea7a6cfec54b0d07483ed1cb503d1ea26d
James Dunne <james.jdunne@gmail.com>
2022-01-31 00:17:11 -0600

    wasm: test: demonstrate Go string passing and copying

--- bsnes/wasm/test2/test2.go
+++ bsnes/wasm/test2/test2.go
@@ -1,12 +1,19 @@
 package main
 
 import "unsafe"
+import "reflect"
 
 //export ppux_draw_list_clear
 func ppux_draw_list_clear()
 
 //export ppux_draw_list_append
 func ppux_draw_list_append(size uint32, cmdlist unsafe.Pointer) uint32
+
+//export za_file_locate
+func za_file_locate(i_filename *byte) int32
+
+//export ppux_font_load_za
+func ppux_font_load_za(i_fontindex int32, i_za_fh int32)
 
 const (
 	COLOR_STROKE = iota
@@ -48,15 +55,36 @@ const (
 //export snes_bus_read
 func snes_bus_read(i_address uint32, i_data unsafe.Pointer, i_size uint32)
 
+var loaded bool
+
+func str(s string) *byte {
+    hdr := (*reflect.StringHeader)(unsafe.Pointer(&s))
+    pbyte := (*byte)(unsafe.Pointer(hdr.Data))
+    return pbyte
+}
+
 //export on_nmi
 func on_nmi() {
+    if !loaded {
+        // load PCF font from ZIP archive:
+        fh := za_file_locate(str("kakwafont-12-n.pcf"))
+        ppux_font_load_za(0, fh)
+
+        loaded = true
+    }
+
 	var module uint8
 	snes_bus_read(0x7E0010, unsafe.Pointer(&module), 1)
 
-	var cmd [8]uint16 = [8]uint16{
+	var cmd = [...]uint16{
 		3, CMD_COLOR_DIRECT_BGR555, COLOR_STROKE, 0x1F3F,
 		3, CMD_PIXEL, 12, 118,
+		7, CMD_COLOR_DIRECT_RGB888, COLOR_STROKE, 0x0000, 0xFF00, COLOR_OUTLINE, 0x00FF, 0x0000,
+		8, CMD_TEXT_UTF8, 0, 2,
+		   7, 0, 0, 0, 0,
 	}
+	copy((*(*[7]uint8)(unsafe.Pointer(&cmd[len(cmd)-4])))[:], "jsd1982")
+
 	ppux_draw_list_clear()
 	ppux_draw_list_append(uint32(len(cmd)), unsafe.Pointer(&cmd))
 }
